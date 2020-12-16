a75a9b48d5db1bc11ff5f8d1036c492d70edb2a7
Nataraja G <nataraj.g@pm.me>
2020-12-15 18:41:19 -0800

    Building the beginnings and structure for the LAND and LOVE pages

--- pages/land.js
+++ pages/land.js
@@ -1,20 +1,60 @@
 import MNGLayout from '../components/mnglayout'
 import {
-  Flex,
-  Box,
-  Image,
-  Text,
-  Heading,
-  Stack,
-  Center
+  Flex, Box, Center,
+  Image, Text, Heading,
+  Button,
+  Stack, VStack,
+  Link as ChakraLink
 } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function Land() {
   return (
     <MNGLayout title="LAND">
-      <Center m={9} color="#B895B9">
-        <Text>BHOOMI MATA BLESS</Text>
-      </Center>
+      <Flex w={960} maxW={960} mx="auto">
+        <Stack spacing={24}>
+          <Image w={600} mx="auto" src="/img/MasterNatarajGLand.jpg"
+            alt="Master Nataraj G Land" title="Master Nataraj G Land" />
+
+          <Box textAlign="center">
+            <Heading as="h1" fontSize="108px" fontWeight="400" color="#E652E9">भूमि</Heading>
+            <Heading as="h2" fontSize="63px" fontWeight="400" color="#B895B9">LAND</Heading>
+            <Text fontSize="27px" color="#B895B9" mt={4}>
+              We are not separate from the land. We are living in an abundant universe
+              and our oneness with our Mother Earth and the entire Cosmos is a
+              manifestation of that abundance. As we are never separate from the
+              land, we are never separate from abundance.
+            </Text>
+            <Text fontSize="27px" color="#B895B9" mt={9}>
+              In many parts of the world, we are living on colonized lands. Even though
+              colonial forces may have left and the lands made “free,” the path to truly
+              sovereign living and fully honoring the sanctity of the land and all beings
+              is continually unfolding and requires conscious action to realize.
+            </Text>
+            <Text fontSize="27px" color="#B895B9" mt={9}>
+              We honor the <ChakraLink as={Link} href="/madeontongvaland">Tongva
+              Land</ChakraLink> we are on and share these offerings with you so
+              we may all live in abundance, aligned with our highest and truest
+              Infinite Self, for a New World rooted in the Divine Energies of
+              Peace, Love, Joy, Balance and Gratitude.
+            </Text>
+            <Text fontSize="27px" color="#B895B9" mt={9}>
+              // for the rest of this page -- links to each section:
+              <br/><br/>FOOD - it's from the land. From Mother Earth. Food of different kinds for
+              our body (physical, fruits, vegetables, seeds, nuts) -- mind,
+              soul, heart, spirit.
+              <br/><br/>ART - to feed the mind, heart and soul.
+              <br/><br/>YOGA - to feed it all and keep us connected, guided and protected
+              by our highest and truest SELF -- which is ONE.
+              <br/><br/>// links to all the above
+            </Text>
+            <Text fontSize="27px" color="#B895B9" mt={9}>
+              PENs from Gma Gloria, Lydia Auntie, adrienne maree brown, Sadhguru
+              <br/><br/>Closing photo of the leaves
+            </Text>
+          </Box>
+        </Stack>
+      </Flex>
     </MNGLayout>
   )
 }
--- pages/love.js
+++ pages/love.js
@@ -12,9 +12,29 @@ import {
 export default function Love() {
   return (
     <MNGLayout title="LOVE">
-      <Center m={9} color="#B895B9">
-        <Text>LOT OF LOVE BRINGING THIS TOGETHER</Text>
-      </Center>
+      <Flex w={960} maxW={960} mx="auto">
+        <Stack spacing={24}>
+          <Image w={600} mx="auto" src="/img/MasterNatarajGLove.jpg"
+            alt="Master Nataraj G Love" title="Master Nataraj G Love" />
+
+          <Box textAlign="center">
+            <Heading as="h1" fontSize="108px" fontWeight="400" color="#E652E9">प्यार</Heading>
+            <Heading as="h2" fontSize="63px" fontWeight="400" color="#B895B9">LOVE</Heading>
+            <Text fontSize="27px" color="#B895B9" mt={4}>
+              We are in it.
+              <br/>It is all around us.
+              <br/>It is us.
+              <br/>If we sit quietly
+              <br/>And listen.
+              <br/><br/><strong>Deep gratitude</strong> to the land, Mother Earth, the Infinite
+                One Self, family, friends, teachers, spirit guides, lovers,
+                Eucalyptus, Cedar, Hummingbird, Dragonfly, Deer, Turtle, Sage, Bee.
+              <br/><br/>// listing of people with brief one-line connection msg and learn more links?
+              <br/><br/>CREDITS for site
+            </Text>
+          </Box>
+        </Stack>
+      </Flex>
     </MNGLayout>
   )
 }
