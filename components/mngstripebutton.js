1333fd6220abd7a45d8c9194503ddfaf29f0e4cb
Master Nataraj G <nataraj.g@pm.me>
2021-01-06 14:37:53 -0800

    Adding page for friends and fam yoga training discount & Stripe integration

---
+++ components/mngstripebutton.js
@@ -0,0 +1,61 @@
+import {
+  Box, Button, Text,
+  Link as ChakraLink
+} from '@chakra-ui/react'
+import Link from 'next/link'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe('pk_live_51I323eKOxPIlITGP2wJgqwTN9pU7vnP2cEOxERgqkuqZngs03MY2NvX8pldjcDxSCwxsZMPC58BatendDLtvtMw900QJ48Fpls')
+
+export default function MNGStripeButton({
+  size = "md",
+  label = "ACCEPT"
+}) {
+  var errorMessage = ""
+  const handleClick = async (event) => {
+    console.log('handleClick')
+
+    // Get Stripe.js instance
+    const stripe = await stripePromise
+
+    /*
+     * When the customer clicks on the button, redirect
+     * them to Checkout.
+     */
+    stripe.redirectToCheckout({
+      lineItems: [{price: 'price_1I6Jm8KOxPIlITGPDMjnfBPh', quantity: 1}],
+      mode: 'payment',
+      /*
+       * Do not rely on the redirect to the successUrl for fulfilling
+       * purchases, customers may not always reach the success_url after
+       * a successful payment.
+       * Instead use one of the strategies described in
+       * https://stripe.com/docs/payments/checkout/fulfill-orders
+       */
+      successUrl: window.location.protocol + '//natarajg.vercel.app/yoga/welcome',
+      cancelUrl: window.location.protocol + '//natarajg.vercel.app/yoga',
+    })
+    .then(function (result) {
+      if (result.error) {
+        errorMessage = result.error.message
+      }
+    })
+  }
+
+  return (
+    <Box>
+      <Button
+        bg="#6E0A70"
+        size={size}
+        color="#F8CDF9"
+        px={8}
+        onClick={handleClick}
+        _hover={{
+          background: "#510653"
+      }}>
+        {label}
+      </Button>
+      <Text color="#F8CDF9" align="center">{errorMessage}</Text>
+    </Box>
+  )
+}
---
+++ pages/yoga/friendsnfam.js
@@ -0,0 +1,78 @@
+import MNGLayout from '../../components/mnglayout'
+import MNGStripeButton from '../../components/mngstripebutton'
+import {
+  Flex, Box, Center,
+  Image, Text, Heading,
+  Button,
+  Stack, VStack,
+  Link as ChakraLink
+} from '@chakra-ui/react'
+import Link from 'next/link'
+
+export default function YogaFriendsNFam() {
+  return (
+    <MNGLayout title="FRIENDS &amp; FAM YOGA">
+      <Flex w={960} maxW={960} mx="auto">
+        <Stack spacing={24}>
+          <Image w={600} mx="auto" src="/img/MasterNatarajGYoga.jpg"
+            alt="Master Nataraj G Yoga" title="Master Nataraj G Yoga" />
+
+          <Box textAlign="center">
+            <Heading as="h1" fontSize="108px" fontWeight="400" color="#E652E9">योग</Heading>
+            <Heading as="h2" fontSize="63px" fontWeight="400" color="#B895B9">YOGA</Heading>
+            <Text fontSize="27px" color="#B895B9" mt={4}>
+              Dear Friends &amp; Family,
+              <br/><br/>Thank you for your love, support and commitment to learn
+              together.
+              <br/><br/>We honor the <ChakraLink as={Link} href="/madeontongvaland">Tongva
+              Land</ChakraLink> we are on and share these teachings with you so
+              we may all live in abundance, aligned with our highest and truest
+              Infinite Self, for a New World rooted in the Divine Energies of
+              Peace, Love, Joy, Balance and Gratitude.
+              <br/><br/>Please click below for your special discounted rate of
+              USD $1,000 for 1 month of 1-on-1 Yoga Training.
+            </Text>
+            <MNGStripeButton size="lg" label="ACCEPT $1,000 OFFER" />
+          </Box>
+
+          <Flex>
+            <VStack align="flex-end" spacing={6} w="593px" mr={6}>
+              <Text textAlign="right" fontSize="36px" fontWeight="bold"
+                color="#F8CDF9">
+                1-on-1 Yoga Training, 1 month
+              </Text>
+              <Text textAlign="right" fontSize="27px" color="#B895B9">
+                Share your life's journey, dreams and intentions with Master
+                Nataraj G and receive a customized schedule and complete Yogic
+                practices for one month tailored for you.
+              </Text>
+              <Text textAlign="right" fontSize="27px" color="#B895B9">
+                This usually takes form as one 30-minute initial conversation
+                followed by 16 one-hour guided sessions with four sessions per
+                week for four weeks and may be adjusted to your needs.
+              </Text>
+              <Text textAlign="right" fontSize="27px" color="#B895B9">
+                All sessions may be conducted virtually.
+              </Text>
+            </VStack>
+
+            <VStack align="center" spacing={4}>
+              <Image w={327} borderRadius="full" mt={4}
+                src="/img/yoga/MNGYogaTraining1mo.jpg"
+                alt="Master Nataraj G 1-on-1 Yoga Training for 1 month"
+                title="Master Nataraj G 1-on-1 Yoga Training for 1 month"
+                boxShadow="0 0 10px 3px rgba(252,255,0,0.5)"
+                border="0.1618rem solid #F2B90B"
+              />
+              <Text color="#F8CDF9" fontSize="27px">
+                USD <Text as="span" fontWeight="bold" fontSize="36px">$1,000</Text>
+              </Text>
+              <MNGStripeButton size="lg" label="ACCEPT" />
+            </VStack>
+          </Flex>
+
+        </Stack>
+      </Flex>
+    </MNGLayout>
+  )
+}
