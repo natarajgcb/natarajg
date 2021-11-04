6a082cad0febf89a086c3b652813dfec190b4162
Nataraj G <73916216+natarajgcb@users.noreply.github.com>
2021-11-04 10:39:58 -0700

    Quick fix for Gauralok planet overlap on mobile

--- components/MNGFlyingHigh.js
+++ components/MNGFlyingHigh.js
@@ -6,7 +6,7 @@ export const MNGFlyingHigh = (props) => {
   return (
     <Box>
       <MNGDivineEagleBless />
-      <Box position="absolute" top="9rem" left="50%" marginLeft="-148px">
+      <Box position="absolute" top={["3rem", "6rem", "9rem"]} left="50%" marginLeft="-148px">
         <MNGHome />
       </Box>
     </Box>
