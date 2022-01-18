165feff14e891d83916aedf000dfe62298730699
Nataraj G <73916216+natarajgcb@users.noreply.github.com>
2022-01-17 19:13:51 -0800

    Activating Gauralok image and text at top of page to lead Home when clicked

--- components/MNGFlyingHigh.js
+++ components/MNGFlyingHigh.js
@@ -1,13 +1,18 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Link as ChakraLink } from '@chakra-ui/react'
 import { MNGHome } from './MNGHome'
 import { MNGDivineEagleBless } from './MNGDivineEagleBless'
+import NextLink from 'next/link'
 
 export const MNGFlyingHigh = (props) => {
   return (
     <Box>
       <MNGDivineEagleBless />
       <Box position="absolute" top={["3rem", "6rem", "9rem"]} left="50%" marginLeft="-148px">
-        <MNGHome />
+        <NextLink href="/" passHref>
+          <ChakraLink>
+            <MNGHome />
+          </ChakraLink>
+        </NextLink>
       </Box>
     </Box>
   )
