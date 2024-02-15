12aaade40e65179f891e59b285c881b8b1903b35
Nataraj G <73916216+natarajgcb@users.noreply.github.com>
2024-02-15 05:55:22 +0000

    Adding COVID DREAMS 95 intro to home page

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import { MNGButton } from '../components/MNGButton'
 import { MNGPage } from '../components/MNGLayout'
 import {
   MNGBody,
-  MNGHindiHeading,
-  MNGHindiTitle,
-  MNGLandLaunchpad,
+  MNGH1,
+  MNGH2,
+  MNGH3,
   MNGHummingbirdSpacer
 } from '../components/MNGType'
 
@@ -24,6 +24,18 @@ export default function Home() {
 
   return (
     <MNGPage meta={mngprops.meta} og={mngprops.og}>
+      <Image mt="mngr.17" width="100%"
+        src="/img/MNGButterfly.jpeg"
+        alt="Photo of a Monarch Butterfly enjoying flowers in a beautiful garden"
+        title="Butterfly Beacon" />
+        
+      <MNGH1 marginBottom="0" marginTop="mngr.27">COVID DREAMS 95</MNGH1>
+      <MNGH3 marginBottom="mngr.27" marginTop="0">Creating New Worlds. For the kids. For the ancestors.</MNGH3>
+      <MNGBody>A collaborative project bringing together storytelling from around the world to create a new world where many worlds harmoniously flow together for Peace, Love and Joy for all beings.</MNGBody>
+      <MNGBody>Subscribe to our mailing list to learn more.</MNGBody>
+      
+      <MNGHummingbirdSpacer />
+
       <Image mt="mngr.17" width="100%"
         src="/img/MasterNatarajGFlower.jpg"
         alt="Photograph of Master Nataraj G"
