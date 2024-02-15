2527e24028736d22d96d66607ee0da06e9b037a8
Nataraj G <73916216+natarajgcb@users.noreply.github.com>
2024-02-15 19:53:10 +0000

    Completing new landing page w/all features in

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
         </MNGWordsOfLoveText>
       </MNGWordsOfLove>
 
-      <MNGHummingbirdSpacer />
+      <MNGHummingbirdSpacer w="50%" />
       
       <Image mt="mngr.17" width="100%"
         src="/img/MNGButterfly.jpeg"
@@ -51,7 +51,7 @@ export default function Home() {
       <MNGBody>An open, collaborative new media platform bringing together storytelling from around the Cosmos to create a New World where many worlds harmoniously flow together for Peace, Love and Joy for All Beings.</MNGBody>
       <MNGBody><a href="#mailingList">Subscribe to our mailing list</a> to learn more and build together.</MNGBody>
 
-      <MNGHummingbirdSpacer />
+      <MNGHummingbirdSpacer w="50%" />
 
       <Box w="100%" mt="mngr.88">
         <div style={{
@@ -71,46 +71,43 @@ export default function Home() {
       <MNGH3 marginBottom="mngr.27" marginTop="0">Want to create together?</MNGH3>
       <MNGBody><a href="mailto:one@natarajg.space?subject=Creating%20With%20Master%20Nataraj%20G">Contact us</a> and let us know the kinds of stories you are most passionate about telling and the change you long to see in the world.</MNGBody>
 
-      <MNGHummingbirdSpacer />
+      <MNGHummingbirdSpacer w="50%" />
 
       <Image mt="mngr.17" width="100%"
-        src="/img/MNGButterfly.jpeg"
+        src="/img/MNGAyurveda.jpeg"
         alt="Photo of a Monarch Butterfly enjoying flowers in a beautiful garden"
         title="Butterfly Beacon" />
 
       <MNGH1 marginBottom="0" marginTop="mngr.27">We Serve People and Planet</MNGH1>
       <MNGH3 marginBottom="mngr.27" marginTop="0">May all enjoy the tastiest, healthiest food that is also healing for Mother Earth.</MNGH3>
-      <MNGBody>Community gardening, growing food ourselves and sharing natural, holistic medicine keeps us active and healthy individually, collectively, is beneficial for the planet and creates a new infinitely abundant economy aligned with the infinite abundance of Mother Earth.</MNGBody>
+      <MNGBody>Community gardening, growing food ourselves and sharing natural, holistic medicine keeps us active and healthy individually, collectively, is beneficial for the planet and creates a new infinitely abundant economy aligned with the infinite abundance of Mother Earth and the Universe.</MNGBody>
       <MNGBody>We have been gardening in community for the past few years, handling 600 lbs of fresh food each week donated by community gardeners, local farmers and health food stores to share with community in the form of healthy snacks and meals informed by Ayurvedic holistic medicine.</MNGBody>
       <MNGBody><a href="#mailingList">Subscribe to our mailing list</a> to garden, grow and eat together.</MNGBody>
 
-      <MNGHummingbirdSpacer />
+      <MNGHummingbirdSpacer w="50%" />
 
-      <Image mt="mngr.17" width="100%"
-        src="/img/MNGButterfly.jpeg"
-        alt="Photo of a Monarch Butterfly enjoying flowers in a beautiful garden"
-        title="Butterfly Beacon" />
+      <Box w="100%" mt="mngr.88">
+        <div style={{
+          padding:"56.25% 0 0 0",
+          position:"relative"
+        }}><iframe src="https://player.vimeo.com/video/290794636?h=bb51e4a977&amp;title=0" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{
+            position:"absolute",
+            top:"0",
+            left:"0",
+            width:"100%",
+            height:"100%"
+          }}></iframe>
+        </div>
+      </Box>
 
-      <MNGH1 marginBottom="0" marginTop="mngr.27">Film / Digital [Course]</MNGH1>
-      <MNGH3 marginBottom="mngr.27" marginTop="0">Food as Medicine: Community Gardening and Ayurvedic cooking.</MNGH3>
-      <MNGBody>We have been gardening in community for the past few years, handling 600 lbs of fresh food each week donated by community gardeners, local farmers and health food stores to share with community in the form of healthy snacks and meals informed by Ayurvedic holistic medicine.</MNGBody>
-      <MNGBody><a href="#mailingList">Contact us</a> to learn more and create together.</MNGBody>
+      <MNGH1 marginBottom="0" marginTop="mngr.27">We Make Digital Interactive</MNGH1>
+      <MNGH3 marginBottom="mngr.27" marginTop="0">Experiences that engage, empower and enlighten.</MNGH3>
+      <MNGBody>The Internet and digital technologies foster an open, inclusive space that connect people locally and globally in new ways. Websites, Mobile Apps, Metaverse, Decentralized Sovereign Web3 Infrastructure... we got it all.</MNGBody>
+      <MNGBody><a href="mailto:one@natarajg.space?subject=Creating%20With%20Master%20Nataraj%20G">Contact us</a> and let us know the what you are building and would like to share with the world.</MNGBody>
 
-      <MNGHummingbirdSpacer />
+      <MNGHummingbirdSpacer w="50%" />
 
-      <Image mt="mngr.17" width="100%"
-        src="/img/MNGButterfly.jpeg"
-        alt="Photo of a Monarch Butterfly enjoying flowers in a beautiful garden"
-        title="Butterfly Beacon" />
-
-      <MNGH1 marginBottom="0" marginTop="mngr.27">Archives [Film]</MNGH1>
-      <MNGH3 marginBottom="mngr.27" marginTop="0">Food as Medicine: Community Gardening and Ayurvedic cooking.</MNGH3>
-      <MNGBody>We have been gardening in community for the past few years, handling 600 lbs of fresh food each week donated by community gardeners, local farmers and health food stores to share with community in the form of healthy snacks and meals informed by Ayurvedic holistic medicine.</MNGBody>
-      <MNGBody><a href="#mailingList">Subscribe to our mailing list</a> to learn more and build together.</MNGBody>
-
-      <MNGHummingbirdSpacer />
-
-      <MNGH1 id="mailinglist">Ready for lift off?</MNGH1>
+      <MNGH1 id="mailingList">Ready for lift off?</MNGH1>
       <script async data-uid="d6f7637e25" src="https://natarajg.ck.page/d6f7637e25/index.js"></script>
 
     </MNGPage>
